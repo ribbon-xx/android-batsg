2ffe30022663928adf0240c502356687e28be534
umbalaconmeogia <umbalaconmeogia@gmail.com>
2013-04-08 05:44:03 +0000

    Add FileUtil and HttpUtil class.

---
+++ android-batsg/src/com/chauhai/android/batsg/util/FileUtil.java
@@ -0,0 +1,183 @@
+package com.chauhai.android.batsg.util;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileFilter;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import android.content.Context;
+import android.util.Log;
+
+/**
+ * Utility functions on file.
+ *
+ * @author umbalaconmeogia
+ *
+ */
+public class FileUtil {
+
+  private static final String TAG = "FileUtil";
+
+	/**
+	 * Get all sub directories and files in specified directory, sort by name.
+	 * @param dirPath
+	 * @param fileFilter If NULL, then sub files are not get.
+	 * @param fileComparator Used to sort the file. May be null.
+	 * @return If the directory does not exist, return null.
+	 *         Else return ArrayList of file.
+	 */
+	public static ArrayList<File> listFiles(String dirPath,
+			FileFilter fileFilter,
+			Comparator<File> fileComparator) {
+
+		// Current directory.
+		File dir = new File(dirPath);
+		// Return null if it is not a directory.
+		if (!dir.exists() || !dir.isDirectory()) {
+			return null;
+		}
+
+		List<File> files;
+		// Get files
+		Log.d(TAG, "listFiles(" + dirPath +")" + fileFilter);
+		files = Arrays.asList(dir.listFiles(fileFilter));
+		if (fileComparator != null) {
+			Collections.sort(files, fileComparator);
+		}
+		// Merge.
+		return new ArrayList<File>(files);
+	}
+
+	/**
+	 * Return the file extension (without dot).
+	 * @param file
+	 * @return
+	 */
+	public static String fileExtension(File file) {
+		String fileExt = null;
+		String fileName = file.getName();
+		int dotPosition = fileName.lastIndexOf(".");
+		if (dotPosition > 0) {
+			fileExt = fileName.substring(dotPosition + 1, fileName.length());
+		}
+		return fileExt;
+	}
+
+	/**
+	 * Get the file name without extension.
+	 * @param file
+	 * @return
+	 */
+	public static String fileNameWithoutExtension(File file) {
+		String fileName = file.getName();
+		String bareName = fileName;
+		int dotPosition = fileName.lastIndexOf(".");
+		if (dotPosition > 0) {
+			bareName = fileName.substring(0, dotPosition);
+		}
+		return bareName;
+	}
+
+	/**
+	 * Save a file form InputStream to a file in the Internal Storage.
+	 * @param context
+	 * @param in
+	 * @param internalStorageFilePath
+	 * @throws IOException
+	 */
+  public static void save(Context context, InputStream in, String internalStorageFilePath) throws IOException {
+    String fileName = context.getFilesDir() + "/" + internalStorageFilePath;
+    save(in, fileName);
+  }
+
+  /**
+   * Save a file from InputStream.
+   * @param in
+   * @param fileName
+   * @throws IOException
+   */
+  public static void save(InputStream in, String fileName) throws IOException {
+    FileOutputStream out = new FileOutputStream(fileName);
+    byte[] buffer = new byte[4096];
+    int len = 0;
+    while ((len = in.read(buffer)) > 0 ) {
+     out.write(buffer,0, len);
+    }
+    out.close();
+  }
+
+  /**
+   * Get content of a file.
+   * @param filePath
+   * @return
+   * @throws IOException
+   */
+	public static String getContents(String filePath) throws IOException {
+	  return getContents(new FileReader(filePath));
+	}
+
+	/**
+	 * Get content of a file in internal storage.
+	 * @param context
+	 * @param internalStorageFileName
+	 * @return
+	 * @throws IOException
+	 */
+	public static String getContents(Context context,
+	    String internalStorageFileName) throws IOException {
+	  return getContents(new InputStreamReader(context.openFileInput(internalStorageFileName)));
+	}
+
+	/**
+	 * Get content from a reader.
+	 * This will close reader.
+	 * @param reader
+	 * @return
+	 * @throws IOException
+	 */
+	public static String getContents(Reader reader) throws IOException {
+    StringBuilder text = new StringBuilder();
+    BufferedReader br = new BufferedReader(reader);
+    String line;
+    while ((line = br.readLine()) != null) {
+      text.append(line);
+      text.append("\n");
+    }
+    reader.close();
+    return text.toString();
+	}
+
+	/**
+   * Get content from a reader.
+	 * @param in
+	 * @return
+	 * @throws IOException
+	 */
+	public static String getContents(InputStream in) throws IOException {
+	  return getContents(new InputStreamReader(in));
+	}
+
+	/**
+	 * Write a string to file.
+	 * @param filePath
+	 * @param content
+	 * @throws IOException
+	 */
+	public static void filePutContents(String filePath, String content) throws IOException {
+		FileWriter fileWriter = new FileWriter(filePath);
+		fileWriter.write(content);
+		fileWriter.flush();
+		fileWriter.close();
+	}
+}
---
+++ android-batsg/src/com/chauhai/android/batsg/util/HttpUtil.java
@@ -0,0 +1,104 @@
+package com.chauhai.android.batsg.util;
+
+import java.io.InputStream;
+import java.net.URI;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+
+import android.util.Log;
+
+public class HttpUtil {
+
+  private static final String TAG = "HttpUtil";
+
+  /**
+   * Connection time out in millisecond.
+   */
+  public static int connectionTimeOutMillisecond = 10000;
+
+  public static String download(String url) throws Exception {
+    return download(url, null);
+  }
+
+  /**
+   * Download a file and save to file path.
+   * If saveFilePath is not specified, then return the content as string.
+   * @param url
+   * @param saveFilePath
+   * @return String if saveFilePath is not specified, null if save to file.
+   * @throws Exception
+   */
+  public static String download(String url, String saveFilePath) throws Exception {
+    Log.d(TAG, "Download " + url);
+
+    // Return content (if saveFilePath is not specified).
+    String returnStr = null;
+
+    DefaultHttpClient client = createHttpClient();
+    client.getParams().setParameter(CoreProtocolPNames.USER_AGENT, System.getProperty("http.agent"));
+
+    HttpGet request = new HttpGet();
+    request.setURI(new URI(url));
+
+    HttpResponse response = client.execute(request);
+    Log.d(TAG, "Download response: " + response.getStatusLine());
+    if (response.getStatusLine().getStatusCode() != HttpStatus.SC_OK) {
+      throw new Exception("Error downloading file " + url);
+    }
+
+    // Save content to file.
+    InputStream in = response.getEntity().getContent();
+    if (saveFilePath != null) {
+      FileUtil.save(in, saveFilePath);
+    } else {
+      returnStr = FileUtil.getContents(in);
+    }
+
+    in.close();
+    in = null;
+
+    return returnStr;
+  }
+
+  /**
+   * Create HttpClient.
+   * @return
+   */
+  public static DefaultHttpClient createHttpClient() {
+    // Set timeout.
+    HttpParams params = new BasicHttpParams();
+    HttpConnectionParams.setConnectionTimeout(params, connectionTimeOutMillisecond);
+    HttpConnectionParams.setSoTimeout(params, connectionTimeOutMillisecond);
+    // Create http client.
+    DefaultHttpClient client = new DefaultHttpClient(params);
+    return client;
+  }
+
+  /**
+   * Create HttpClient with Basic Authentication.
+   * @param authHost
+   * @param authPort
+   * @param authUser
+   * @param authPassword
+   * @return
+   */
+  public static DefaultHttpClient createHttpClient(String authHost, int authPort,
+      String authUser, String authPassword) {
+    // Create http client.
+    DefaultHttpClient client = createHttpClient();
+    // Set authentication parameters if specified.
+    client.getCredentialsProvider().setCredentials(
+        new AuthScope(authHost, authPort),
+        new UsernamePasswordCredentials(authUser, authPassword));
+    return client;
+  }
+}
